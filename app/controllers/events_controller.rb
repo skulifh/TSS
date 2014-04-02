c4c58a574c1f1f640010ed7ed61a7797ee477412
Skuli Freyr Hinriksson <skuli@sareye.com>
2014-04-02 20:12:13 +0200

    initial commit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,45 @@
+require 'socket'
+class EventsController < ApplicationController
+  def index
+
+  end
+
+  def show
+  	@event = Event.find(params[:id])
+  end
+
+  def purchase
+  	@event = Event.find(params[:id])
+  end
+
+  def confirm_purchase
+  	@event = Event.find(params[:id])
+  	a = TCPSocket.new('46.149.22.251', 2000) # could replace 127.0.0.1 with your "real" IP if desired.
+	a.write "jon"
+	@reply = a.recv(1024)
+	a.close
+
+  end
+
+  def create
+  	Event.create(event_params)
+  end
+
+  def update
+    redirect_to current_account.event.find(params[:id]).tap { |event|
+      event.update!(event_params)
+    }
+  end
+
+  def destroy
+        @event = Event.find(params[:event_id])
+        @event.destroy
+        redirect_to :back
+  end
+
+  private
+
+  def event_params
+  	params.require(:event).permit(:name, :description, :price)
+  end
+end
---
+++ config/initializers/.svn/text-base/secret_token.rb.svn-base
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TSS::Application.config.secret_key_base = '6e7b8a632754c098ec548a830a262830fcfd739c1f456f0b11a8272bcb4b555a869366b0751b9ead33404e943c614e0fe2c9dd6feaf0fc2d30c229a7c9e30e16'
