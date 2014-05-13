890ae9f12455316ba09d72541d6d46c6da2b62ff
Skuli Freyr Hinriksson <skuli@sareye.com>
2014-05-13 13:44:53 +0200

    added credit card input field

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,10 +22,11 @@ class EventsController < ApplicationController
 
   def confirm_purchase
   	@event = Event.find(params[:id])
+    credit_card = params[:creditcard][:number]
 
 
   	a = TCPSocket.new('46.149.22.251', 2000) # could replace 127.0.0.1 with your "real" IP if desired.
-	  a.write "jon"
+	  a.write credit_card
 	  @reply = a.recv(1024)
 	  a.close
 
